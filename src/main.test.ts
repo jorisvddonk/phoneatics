c52eeb3db41133b7e1c995e66d4b3b1d561461ae
joris <jorisvddonk@gmail.com>
2019-11-07 21:21:55 +0100

    mock lodash random

--- src/main.test.ts
+++ src/main.test.ts
@@ -1,4 +1,7 @@
 import Phoneatics from './main';
+import random from 'lodash.random';
+
+jest.mock('lodash.random');
 
 describe("Phoneatics", () => {
   let phoneatics;
@@ -7,17 +10,22 @@ describe("Phoneatics", () => {
   })
 
   it("should return a string when given a pattern", () => {
-    expect(phoneatics.generate("Sv")).toBeDefined();
+    random.mockReturnValueOnce(0);
+    random.mockReturnValueOnce(0);
+    expect(phoneatics.generate("Sv")).toBe("Pa");
   })
 
   it("should return a number between 0 and 9 when given a 'u' pattern character", () => {
     // todo: mock lodash random function
-    expect(phoneatics.generate("u")).toMatch(/[0-9]/);
+    random.mockReturnValueOnce(0);
+    expect(phoneatics.generate("u")).toBe("0");
+    expect(random).toHaveBeenCalledWith(0, 9);
   })
 
   it("should return a number between 1 and 9 when given a 'U' pattern character", () => {
-    // todo: mock lodash random function
-    expect(phoneatics.generate("U")).toMatch(/[1-9]/);
+    random.mockReturnValueOnce(4);
+    expect(phoneatics.generate("U")).toBe("4");
+    expect(random).toHaveBeenCalledWith(1, 9);
   })
 
   it("should return a literal string when given literal escape characters", () => {
